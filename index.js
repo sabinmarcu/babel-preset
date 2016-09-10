98674accaed34959fb0f504e2e216a01648af04b
Sabin Marcu <sabinmarcu@gmail.com>
2016-09-10 16:44:43 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "presets": [
+        "es2015",
+        "stage-0",
+        "react"
+    ],
+    "plugins": [
+        "transform-flow-strip-types",
+        "add-module-exports",
+        "transform-decorators-legacy"
+    ]
+};
